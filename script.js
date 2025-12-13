83b42998aa183da053f8e82b391a1212912996a2
Rhcoder79 <robiulhasan7972171@gmail.com>
2025-12-13 21:13:19 +0600

    card funtionality added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
  console.log('i am connected');
+ let allPlansData=[];
 const loadCategory=()=>{
   const url='https://openapi.programming-hero.com/api/categories';
   fetch(url)
@@ -7,17 +8,13 @@ const loadCategory=()=>{
   
 }
 
- const matched=(id)=>{
- console.log(id);
- }
-
  const displayCategory=(categoryName)=>{
-  
+    
 //     const categories=categoryName.categories
      const allCategory=document.getElementById("all-category");
     allCategory.innerHTML='';
     for(let name of categoryName){
-     console.log(name);
+   //  console.log(name);
          const categoryList=document.createElement('li');
          
        categoryList.innerHTML= `
@@ -28,62 +25,50 @@ const loadCategory=()=>{
     }
  } 
  loadCategory();
-// const displayCategory=(categoryName)=>{
-//     const categories=categoryName.categories
-//     const allCategory=document.getElementById("all-category");
-//     allCategory.innerHTML='';
-//     for(const name of categoryName){
-//         const categoryList=document.createElement('li');
-//         categoryList.innerHTML = `
-//         <a  href="">${name.category_name}</a>
-//         `;
-//      categoryList.addEventListener('click', (matched) => {
-  
-//           //  filterCards(name.category_name);
-//           if(name.category_name===cardContainer.category){
-//             return cardContainer.cardDiv;
-//           }
-//         });
-        
+const loadCard=()=>{
+    const url='https://openapi.programming-hero.com/api/plants';
+    fetch(url)
+    .then(res=>res.json()) 
+    .then(result=>{
+      allPlansData=result.plants;
+      allCards(allPlansData);
+    });
+}
+const allCards=(cardInfo)=>{
+ // const plants=cardInfo.plants;
+  const cardContainer=document.getElementById("card-container");
+  cardContainer.innerHTML='';
+  for(const card of cardInfo){
+    const cardDiv=document.createElement('div');
+    cardDiv.innerHTML=`
+        <div class="cards">
+        <div><img src="${card.image}" alt="image not fount"></div>
+     <div >
+    <h3 >${card.name}</h3>
+    <p>${card.description }</p>
+    <div>
+        <label for="">${card.category}</label>
+        <h3>${card.price}</h3>
+    </div>
+           <button   class="btn bg-[#15803D] text-white  mt-5 w-full rounded-3xl" >Add To Card</button>
 
-//         allCategory.append(categoryList);
- 
-//       }
-//  }
-
- 
-// const loadCard=()=>{
-//     const url='https://openapi.programming-hero.com/api/plants';
-//     fetch(url)
-//     .then(res=>res.json()) 
-//     .then(result=>allCards(result));
-// }
-// const allCards=(cardInfo)=>{
-//   const plants=cardInfo.plants;
-//   const cardContainer=document.getElementById("card-container");
-//   cardContainer.innerHTML='';
-//   for(const card of plants){
-//     const cardDiv=document.createElement('div');
-//     cardDiv.innerHTML=`
-//         <div class="cards">
-//         <div><img src="${card.image}" alt="image not fount"></div>
-//      <div >
-//     <h3 >${card.name}</h3>
-//     <p>${card.description }</p>
-//     <div>
-//         <label for="">${card.category}</label>
-//         <h3>${card.price}</h3>
-//     </div>
-//            <button   class="btn bg-[#15803D] text-white  mt-5 w-full rounded-3xl" >Add To Card</button>
-
-// </div>
-//      </div>
-//     `
-//     cardContainer.append(cardDiv);
+</div>
+     </div>
+    `; 
+    cardContainer.append(cardDiv);
    
-//   }
-   
-// }
-// loadCard();
+  }
+ 
+}
+loadCard();
+
+
+const matched=(categoryName)=>{
+const filterPlants=allPlansData.filter(card=>card.category===categoryName);
+allCards(filterPlants);
+
+
+ }
+
 
 
