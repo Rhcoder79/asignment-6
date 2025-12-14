ba7643879869627f261317c92ab1636e46552297
Rhcoder79 <robiulhasan7972171@gmail.com>
2025-12-14 12:39:58 +0600

    modal complete

--- script.js
+++ script.js
@@ -33,7 +33,33 @@ const loadCard=()=>{
       allPlansData=result.plants;
       allCards(allPlansData);
     });
+};
+const loadPlantDetail=async(id)=>{
+  const url=`https://openapi.programming-hero.com/api/plant/${id}`;
+  //console.log(url);
+  const res=await fetch(url);
+  const details=await res.json();
+displayPlantDetails(details.plants);
 }
+// {id: 5, image: 'https://i.ibb.co.com/qY8qS7YN/champa-min.jpg',
+// name: 'Champa', description: 'A fragrant flowering tree that adorns gardens with…y cherished in traditional rituals and
+//  perfumery.',
+//    category: 'Flowering Tree', …}
+const displayPlantDetails=(plant)=>{
+console.log(plant);
+const detailsCard=document.getElementById("details-container");
+detailsCard.innerHTML=`
+<h3>${plant.name}</h3>
+        <div><img src="${plant.image}" alt="image not fount"></div>
+        <p>${plant.description}</p>
+        <p>${plant.category}</p>
+        <h3>${plant.price}</h3>
+`;
+document.getElementById("plant_modal").showModal();
+
+}
+
+
 const allCards=(cardInfo)=>{
  // const plants=cardInfo.plants;
   const cardContainer=document.getElementById("card-container");
@@ -44,7 +70,7 @@ const allCards=(cardInfo)=>{
         <div class="cards">
         <div><img src="${card.image}" alt="image not fount"></div>
      <div >
-    <h3 >${card.name}</h3>
+    <h3 onclick="loadPlantDetail(${card.id})" >${card.name}</h3>
     <p>${card.description }</p>
     <div>
         <label for="">${card.category}</label>
@@ -61,6 +87,9 @@ const allCards=(cardInfo)=>{
  
 }
 loadCard();
+// const modal=()=>{
+//   fetch()
+// }
 
 
 const matched=(categoryName)=>{
@@ -69,6 +98,5 @@ allCards(filterPlants);
 
 
  }
-
 
 
